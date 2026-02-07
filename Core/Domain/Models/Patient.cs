1863f791e33281dc50ba8b461fff0961d754cccb
Belal <BelalGit@gmail.com>
2026-02-07 11:47:12 +0200

    patient table

---
+++ Core/Domain/Models/Patient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Patient
+    {
+        public string  Name { get; set; }
+
+    }
+}
