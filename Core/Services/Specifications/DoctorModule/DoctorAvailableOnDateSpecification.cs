3b9776afe7e74c0ed48795b05b6cbe2ae19c7253
Belal <BelalGit@gmail.com>
2026-02-26 08:21:19 +0200

    AppointmentFullApiService

--- Core/Services/Specifications/DoctorModule/DoctorAvailableOnDateSpecification.cs
+++ Core/Services/Specifications/DoctorModule/DoctorAvailableOnDateSpecification.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Models.DoctorModule;
 using Domain.Models.Enums.DoctorEnums;
+using DayOfWeek = Domain.Models.Enums.DoctorEnums.DayOfWeek;
 
 
 namespace Services.Specifications.DoctorModule
@@ -7,14 +8,19 @@ namespace Services.Specifications.DoctorModule
     public class DoctorAvailableOnDateSpecification:BaseSpecifications<Doctor,int>
     {
         public DoctorAvailableOnDateSpecification(DateTime date)
-            : base(d => d.Status == DoctorStatus.Active &&
-            d.AvailabilitySchedules.Any(s =>
-                s.IsAvailable &&
-                s.DayOfWeek == (Domain.Models.Enums.DoctorEnums.DayOfWeek)
-                    (date.DayOfWeek == 0 ? 7 : date.DayOfWeek)))
+            : base(BuildCriteria(date))
         {
             AddInclude(d => d.Department);
             AddInclude(d => d.AvailabilitySchedules);
+        }
+
+        private static System.Linq.Expressions.Expression<Func<Doctor, bool>> BuildCriteria(DateTime date)
+        {
+            var customDay = (DayOfWeek)((int)date.DayOfWeek == 0 ? 7 : (int)date.DayOfWeek);
+            return d => d.Status == DoctorStatus.Active &&
+                        d.AvailabilitySchedules.Any(s =>
+                            s.IsAvailable &&
+                            s.DayOfWeek == customDay);
         }
     }
 }
